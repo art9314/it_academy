60abb5dddd40c25483bd2be24249779148cd961f
art9314 <art9314@mail.ru>
2022-10-20 00:16:14 +0300

    'local1'

--- src/reference_book/forms.py
+++ src/reference_book/forms.py
@@ -2,9 +2,24 @@ from django import forms
 from django.core.exceptions import ValidationError
 from . import models 
 
-class AuthorForm(forms.Form):
-    name = forms.CharField(max_length=50)
-    date_of_birth = forms.DateField()
-    date_of_death = forms.DateField()
-    description = forms.CharField(widget=forms.Textarea, required = False)
 
+
+def name_validator(value:str):
+    if value.startswith('M'):
+        raise ValidationError('Must not srart')
+    return value
+
+class AuthorForm(forms.ModelForm):
+    class Meta:
+        model=models.Author
+        fields=['name','date_of_birth','date_of_death','description' ]
+        
+        
+        def clean(self):
+            cleaned_data=super().clean()
+            date_of_birth=cleaned_data.get('date_of_birth')
+            date_of_death=cleaned_data.get('date_of_death')
+            if date_of_death<date_of_birth:
+                self.add_error('date_of_birth','< than date_of_death')
+                self.add_error('date_of_birth','> than date_of_birth')
+            return cleaned_data
